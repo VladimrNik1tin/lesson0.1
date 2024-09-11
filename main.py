e0c70e6e8bbb3134711a434a45596167edd3e048
VladimrNik1tin <nikitin.vladimir99@mail.ru>
2024-09-11 10:47:39 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ print(9 ** 0.5 * 5)
 # 2nd program
 print(9.99 > 9.98 and 1000 != 1000.1)
 # 3rd program
-print(a := 2 * 2 + 2)
-print(b := 2 * (2 + 2))
-print(a == b)
+print(1234 % 1000 // 10 + int(5678 % 1000 // 10))
 # 4th program
-print(int(float('123.456')*10)%10)
+print(13.42 * 100 // 100 == 42.13 * 100 % 100 or 13.42 * 100 % 100 == 42.13 * 100 // 100)
